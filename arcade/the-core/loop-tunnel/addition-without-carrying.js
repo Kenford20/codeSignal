394b34d361d233ab39563b6c30c270a066ae21be
Kenford <kzhou0066@gmail.com>
2018-11-15 13:35:38 -0500

    completed the challenges of loop tunnel section

--- arcade/the-core/loop-tunnel/addition-without-carrying.js
+++ arcade/the-core/loop-tunnel/addition-without-carrying.js
@@ -11,7 +11,7 @@ function additionWithoutCarrying(param1, param2) {
     
     for(let i = smallerAddend.length - 1; i >= 0; i--){
         let index = (smallerAddend.length === largerAddend.length) ? i : i+1;
-        littleBoysSum.unshift((parseInt(largerAddend[index]) + parseInt(smallerAddend[i]))%10);
+        littleBoysSum.unshift((parseInt(largerAddend[index]) + parseInt(smallerAddend[i])) % 10);
         
         if(i === 0 && smallerAddend.length !== largerAddend.length)
             littleBoysSum.unshift(largerAddend.slice(0, largerAddend.length - smallerAddend.length));
--- arcade/the-core/loop-tunnel/increase-number-roundness.js
+++ arcade/the-core/loop-tunnel/increase-number-roundness.js
@@ -2,3 +2,8 @@
 
 // Given an integer n, check if it's possible to increase n's roundness by swapping some pair of its digits.
 
+function increaseNumberRoundness(n) {
+    let consecutiveNonZeroes = /^[1-9]+0*$/;
+    
+    return !consecutiveNonZeroes.test(n);
+}
