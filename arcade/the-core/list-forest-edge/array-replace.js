0909f0b4d5c9ce13ff24eb680bd824657a23a264
Kenford <kzhou0066@gmail.com>
2018-11-16 00:20:34 -0500

    completed more challenges

--- arcade/the-core/list-forest-edge/array-replace.js
+++ arcade/the-core/list-forest-edge/array-replace.js
@@ -5,3 +5,10 @@
 // For inputArray = [1, 2, 1], elemToReplace = 1, and substitutionElem = 3, the output should be
 // arrayReplace(inputArray, elemToReplace, substitutionElem) = [3, 2, 3].
 
+function arrayReplace(inputArray, elemToReplace, substitutionElem) {
+    for(let i = 0; i < inputArray.length; i++){
+        if(inputArray[i] === elemToReplace)
+            inputArray.splice(i, 1, substitutionElem);
+    }
+    return inputArray;
+}
---
+++ arcade/the-core/list-forest-edge/remove-array-part.js
@@ -0,0 +1,6 @@
+// Remove a part of a given array between given 0-based indexes l and r (inclusive).
+
+function removeArrayPart(inputArray, l, r) {
+    inputArray.splice(l, r-l+1);
+    return inputArray;
+}
