773a38c09ef125e505d4d149dcdb3ea421bc507f
Kenford <kzhou0066@gmail.com>
2018-11-16 01:01:30 -0500

    completed challenges of list forest edge section

--- arcade/the-core/list-forest-edge/replace-middle.js
+++ arcade/the-core/list-forest-edge/replace-middle.js
@@ -4,3 +4,17 @@
 // if arr contains an even number of elements, its middle is the sum of the two elements whose index numbers when counting from the beginning and from the end of the array differ by one.
 // Given array arr, your task is to find its middle, and, if it consists of two elements, replace those elements with the value of middle. Return the resulting array as the answer.
 
+function makeArrayConsecutive2(statues) {
+    statues.sort((a,b) => a - b);
+    let numStatues = 0;
+    
+    for(let i = 0; i < statues.length; i++){
+        if(statues[i] !== statues[0]){
+            numStatues++;
+            i--;
+        }
+        statues[0]++;
+    }
+    return numStatues;
+}
+
