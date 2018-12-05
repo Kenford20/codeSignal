0ea4af0074c49fe7c83715444093b584e3f1e260
Kenford <kzhou0066@gmail.com>
2018-12-04 23:01:32 -0500

    completed rains of reason secton

--- arcade/intro/rains-of-reason/chess-board-cell-color.js
+++ arcade/intro/rains-of-reason/chess-board-cell-color.js
@@ -8,3 +8,19 @@
 // For cell1 = "A1" and cell2 = "H3", the output should be
 // chessBoardCellColor(cell1, cell2) = false.
 
+function chessBoardCellColor(cell1, cell2) {
+    let letters = 'ABCDEFGH';
+    
+    let isCell1Odd = cell1[1] % 2 === 1 ? true : false; 
+    let isCell2Odd = cell2[1] % 2 === 1 ? true : false; 
+    
+    // receives a cell letter and returns the color of the cell based on whether its pairing number is even or odd
+    function cellColor(letter, isCellNumOdd){
+        if(letters.indexOf(letter) % 2 === 0) { // aceg
+            return isCellNumOdd ? 'dark' : 'light'
+        } else { // bdfh
+            return isCellNumOdd ? 'light' : 'dark';
+        }
+    }
+    return cellColor(cell1[0], isCell1Odd) === cellColor(cell2[0], isCell2Odd);
+}
