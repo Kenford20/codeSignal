1e7612828e0edfd5e24cda1299bdd63ab54ee75c
Kenford <kzhou0066@gmail.com>
2018-12-18 23:45:25 -0500

    started time river section of core challenges

---
+++ arcade/the-core/time-river/valid-time.js
@@ -0,0 +1,14 @@
+// Check if the given string is a correct time representation of the 24-hour clock.
+
+// Example
+
+// For time = "13:58", the output should be
+// validTime(time) = true;
+// For time = "25:51", the output should be
+// validTime(time) = false;
+// For time = "02:76", the output should be
+// validTime(time) = false.
+
+function validTime(time) {
+    return parseInt(time[0])*10 + parseInt(time[1]) < 24 && parseInt(time[3])*10 + parseInt(time[4]) < 60;
+}
