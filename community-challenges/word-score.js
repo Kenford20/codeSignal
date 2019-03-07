571ccfd91aca0378dc7f31512226e423116eb4d3
Kenford <kzhou0066@gmail.com>
2019-03-07 11:52:09 -0500

    completed daily challenge

---
+++ community-challenges/word-score.js
@@ -0,0 +1,20 @@
+// This is a reverse challenge. Good luck!
+// It's a challenge without description , only with some visible tests !
+
+// Input: word: "skills"
+// Expected Output: 82
+
+// Input: word: "knowledge"+
+// Expected Output: 96
+
+// Input: word: "hardwork"
+// Expected Output: 98
+
+// Input: word: "z"
+// Expected Output: 26
+
+function WordScore(word) {
+    let alphabet = '.abcdefghijklmnopqrstuvwxyz';
+    
+    return word.split('').map(letter => alphabet.indexOf(letter)).reduce((a, b) => a + b);
+}
