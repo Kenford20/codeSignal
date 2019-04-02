0d34edb04ea091e467cce11700de102e9babde2c
Kenford <kzhou0066@gmail.com>
2019-04-01 23:55:51 -0400

    completed daily challenge

---
+++ community-challenges/all-longest-strings.js
@@ -0,0 +1,12 @@
+// Given an array of strings, return another array containing all of its longest strings.
+
+// Example
+
+// For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+// allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
+
+function allLongestStrings(inputArray) {
+    let longestLength = Math.max(...inputArray.map(str => str.length));
+    
+    return inputArray.filter(str => str.length === longestLength);
+}
