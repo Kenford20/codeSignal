6d8aba92d5e5dd4490a60fb6d5cfe4dcc4ae8e51
Kenford <kzhou0066@gmail.com>
2019-09-06 19:31:27 -0500

    complete daily challenge

---
+++ community-challenges/from-decimal.js
@@ -0,0 +1,10 @@
+// Transform a number n into the given base b.
+
+// Example
+
+// For b = 2 and n = 13, the output should be
+// fromDecimal(b, n) = "1101".
+
+function fromDecimal(b, n) {
+    return n.toString(b);
+}
